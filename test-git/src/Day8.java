a2329f845b3c29110975c8b9ba2c7ad09bb5988c
dilek-technostudy <dileknuranyildirim@gmail.com>
2019-11-07 13:04:31 -0800

    idid it finally

---
+++ test-git/src/Day8.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Day8 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("provide a day  Monday to Sunday");
+
+        String dayOfWeek = scan.nextLine();
+
+        switch (dayOfWeek) {
+
+            case "Monday":
+                System.out.println("1");
+
+                break;
+
+            case "Tuesday":
+
+                System.out.println("2");
+
+                break;
+
+            case "Wednesday":
+
+                System.out.println("3");
+
+                break;
+
+            case "Thursday":
+
+                System.out.println("4");
+
+                break;
+
+            case "Friday":
+
+                System.out.println("5");
+
+                break;
+
+            case "Saturday":
+
+                System.out.println("6");
+                break;
+
+            case "Sunday":
+
+                System.out.println("7");
+
+
+        }
+
+    }
+}
+
+
+
+
+
+
